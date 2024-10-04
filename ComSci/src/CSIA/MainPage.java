322150dc76aa4e9fd0bc1d3427a664d38523400b
Kyle <Kyle@Desktop.lan>
2024-10-04 23:10:31 +0800

    added text fields

--- ComSci/src/CSIA/MainPage.java
+++ ComSci/src/CSIA/MainPage.java
@@ -87,53 +87,113 @@ public class MainPage extends JFrame {
 		
 		JLabel iilblNewLabel = new JLabel("Item");
 		iilblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		iilblNewLabel.setBounds(10, 541, 51, 30);
+		iilblNewLabel.setBounds(10, 571, 51, 30);
 		contentPane.add(iilblNewLabel);
 		
 		JLabel iblblNewLabel = new JLabel("Brand");
 		iblblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		iblblNewLabel.setBounds(10, 581, 71, 30);
+		iblblNewLabel.setBounds(10, 636, 71, 30);
 		contentPane.add(iblblNewLabel);
 		
 		JLabel idlblNewLabel = new JLabel("Description");
 		idlblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		idlblNewLabel.setBounds(10, 621, 129, 30);
+		idlblNewLabel.setBounds(10, 700, 129, 30);
 		contentPane.add(idlblNewLabel);
 		
 		JLabel iclblNewLabel = new JLabel("Category");
 		iclblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		iclblNewLabel.setBounds(10, 660, 104, 30);
+		iclblNewLabel.setBounds(10, 773, 104, 30);
 		contentPane.add(iclblNewLabel);
 		
 		JLabel iqlblNewLabel = new JLabel("Quantity");
 		iqlblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		iqlblNewLabel.setBounds(10, 700, 97, 30);
+		iqlblNewLabel.setBounds(10, 841, 97, 30);
 		contentPane.add(iqlblNewLabel);
 		
 		JLabel silblNewLabel = new JLabel("Item");
 		silblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		silblNewLabel.setBounds(802, 541, 51, 30);
+		silblNewLabel.setBounds(802, 571, 51, 30);
 		contentPane.add(silblNewLabel);
 		
 		JLabel sblblNewLabel = new JLabel("Brand");
 		sblblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		sblblNewLabel.setBounds(802, 581, 71, 30);
+		sblblNewLabel.setBounds(802, 636, 71, 30);
 		contentPane.add(sblblNewLabel);
 		
 		JLabel sdlblNewLabel = new JLabel("Description");
 		sdlblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		sdlblNewLabel.setBounds(802, 621, 129, 30);
+		sdlblNewLabel.setBounds(802, 700, 129, 30);
 		contentPane.add(sdlblNewLabel);
 		
 		JLabel sclblNewLabel = new JLabel("Category");
 		sclblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		sclblNewLabel.setBounds(802, 660, 104, 30);
+		sclblNewLabel.setBounds(802, 773, 104, 30);
 		contentPane.add(sclblNewLabel);
 		
 		JLabel sqlblNewLabel = new JLabel("Quantity");
 		sqlblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		sqlblNewLabel.setBounds(802, 700, 97, 30);
+		sqlblNewLabel.setBounds(802, 841, 97, 30);
 		contentPane.add(sqlblNewLabel);
+		
+		iitextField = new JTextField();
+		iitextField.setFont(new Font("Arial", Font.PLAIN, 25));
+		iitextField.setBounds(149, 573, 325, 30);
+		contentPane.add(iitextField);
+		iitextField.setColumns(10);
+		
+		ibtextField = new JTextField();
+		ibtextField.setFont(new Font("Arial", Font.PLAIN, 25));
+		ibtextField.setColumns(10);
+		ibtextField.setBounds(149, 636, 325, 30);
+		contentPane.add(ibtextField);
+		
+		idtextField = new JTextField();
+		idtextField.setFont(new Font("Arial", Font.PLAIN, 25));
+		idtextField.setColumns(10);
+		idtextField.setBounds(149, 702, 325, 30);
+		contentPane.add(idtextField);
+		
+		ictextField = new JTextField();
+		ictextField.setFont(new Font("Arial", Font.PLAIN, 25));
+		ictextField.setColumns(10);
+		ictextField.setBounds(149, 773, 325, 30);
+		contentPane.add(ictextField);
+		
+		iqtextField = new JTextField();
+		iqtextField.setFont(new Font("Arial", Font.PLAIN, 25));
+		iqtextField.setColumns(10);
+		iqtextField.setBounds(149, 841, 325, 30);
+		contentPane.add(iqtextField);
+		
+		sitextField = new JTextField();
+		sitextField.setFont(new Font("Arial", Font.PLAIN, 25));
+		sitextField.setColumns(10);
+		sitextField.setBounds(944, 573, 325, 30);
+		contentPane.add(sitextField);
+		
+		sbtextField = new JTextField();
+		sbtextField.setFont(new Font("Arial", Font.PLAIN, 25));
+		sbtextField.setColumns(10);
+		sbtextField.setBounds(944, 636, 325, 30);
+		contentPane.add(sbtextField);
+		
+		sdtextField = new JTextField();
+		sdtextField.setFont(new Font("Arial", Font.PLAIN, 25));
+		sdtextField.setColumns(10);
+		sdtextField.setBounds(944, 702, 325, 30);
+		contentPane.add(sdtextField);
+		
+		sctextField = new JTextField();
+		sctextField.setFont(new Font("Arial", Font.PLAIN, 25));
+		sctextField.setColumns(10);
+		sctextField.setBounds(944, 773, 325, 30);
+		contentPane.add(sctextField);
+		
+		sqtextField = new JTextField();
+		sqtextField.setFont(new Font("Arial", Font.PLAIN, 25));
+		sqtextField.setColumns(10);
+		sqtextField.setBounds(944, 841, 325, 30);
+		contentPane.add(sqtextField);
 
 	}
 }
